ad14b63c8a7ae99b8f8347e20f8ecaff3bb62dc4
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-13 09:12:14 +0530

    app

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 
 
-const app = express()
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://ananthan123:ananthan123@cluster0.4r0z6.mongodb.net/userdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/add",(req,res)=>{
+    let input = req.body
+    let user = new usermodel(input)
+    user.save()
+    console.log(course)
+    res.json({"status":"success"})
+})
+
+app.get("/view",(req,res)=>{
+    usermodel.find().then(
+        (data)=>{
+            res.json(data)
+
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        }
+    )
+})
 
 
 app.listen(8085,()=>{
